d7884d3eb7f1c659d6ef67df483433299b178ad6
Mafruk101 <mafrukhosen1010@gmail.com>
2025-09-13 23:42:56 +0600

    feedback, forgot pass, final commit from customer

--- Hardware_Management_System/Customer/VIEW/customerDashboard.php
+++ Hardware_Management_System/Customer/VIEW/customerDashboard.php
@@ -60,6 +60,23 @@ else{
     $sql = "SELECT * FROM product";
 }
 $result = $conn->query($sql);
+
+
+if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST['FName'], $_POST['FEmail'], $_POST['FMessage'])){
+    $FName= trim($_POST['FName']);
+    $FEmail= trim($_POST['FEmail']);
+    $FMessage=trim($_POST['FMessage']);
+
+    if(empty($FName)|| empty($FEmail)||empty($FMessage)){
+        echo "<script>alert('please fill all the sections for feedback')</script>";
+    }
+    else{
+        $sql="INSERT INTO feedback (FName, FEmail, FMessage) VALUES ('$FName','$FEmail','$FMessage')";
+        if($conn->query($sql)===TRUE){
+            echo "<script>alert('Feedback sent successfully')</script>";
+        }
+    }
+}
 ?>
 
 
@@ -84,12 +101,10 @@ $result = $conn->query($sql);
     <div class="mainContainer" style="display: flex;">
     <div class="dashboardListContainer">
         <ul>
-            <li>Shop</li>
             <li><a href="profile.php">Profile</a></li>
             <li><a href="history.php">History</a></li>
             <li><a href="repairReq.php">Repair</a></li>
-            <li>Feedback</li>
-            <li>Rate us</li>
+            <li><a href="#feedBack">Feedback</a></li>
         </ul>
     </div>
     <div class="productsContainer">
@@ -114,5 +129,109 @@ $result = $conn->query($sql);
         ?>
     </div>
     </div>
+    <div class="footer" id="feedBack">
+        <h1 style="font-size: 50px;">Always<br>
+            choose<br>
+            the <br>
+            best <br>
+            product</h1>
+            
+        <h2 style="padding:35px;"><b>Monitors</b><br>
+        <p style="font-size: 13px;">CRT (Cathode Ray Tube)<br>
+        LCD (Liquid Crystal Display)<br>
+        LED (Light-Emitting Diode)<br>
+        OLED (Organic Light-Emitting Diode)<br>
+        MiniLED<br>
+        QD-OLED<br>
+    </p>
+        Brands
+        <p style="font-size: 13px;">ASUS<br>
+        Dell<br>
+        Samsung<br>
+        Acer<br>
+        ViewSonic<br>
+        HP<br>
+    </p>
+        </h2>
+
+        <h2 style="padding:35px;"><b>keyboards</b><br>
+        <p style="font-size: 13px;">Logitech G PRO X TKL RAPID<br>
+        T-wolf K220 Wired Keyboard<br>
+        Keychron Q5 HE<br>
+        Fantech GO K211 <br>
+        Razer BlackWidow V4 Pro<br>
+        QD-Wooting 80HE<br>
+        Keychron B1 Pro Wireless<br>
+        Keychron V6 Max<br>
+        Havit Kb271 Usb Exquisite<br>
+        SteelSeries Apex Pro Mini<br>
+        Aula F75 Wireless<br>
+        Keychron K2HE<br>
+        NuPhy Air60 HE<br>
+        Keychron K8<br>
+        Aula F75 Wireless<br>
+        NuPhy Air60 HE<br>
+    </p>
+        </h2>
+
+        <h2 style="padding:35px;"><b>keyboards</b><br>
+         <p style="font-size: 13px;">
+        Wired Mouse<br>
+        Wireless Mouse<br>
+        Gaming Mouse<br>
+        Ergonomic Mouse<br>
+        Trackball Mouse<br>
+        Vertical Mouse<br>
+        Portable Travel Mouse<br>
+    </p>
+    Brands
+    <p style="font-size: 13px;">
+        Logitech<br>
+        Razer<br>
+        Corsair<br>
+        SteelSeries<br>
+        Glorious<br>
+        HyperX<br>
+        ASUS ROG<br>
+    </p>
+        </h2>
+
+        </h2>
+
+        <h2 style="padding:35px;"><b>keyboards</b><br>
+        <p style="font-size: 13px;">
+        Ultrabooks<br>
+        Gaming Laptops<br>
+        Business Laptops<br>
+        2-in-1 Convertible Laptops<br>
+        Student Laptops<br>
+        Workstation Laptops<br>
+        Budget Laptops<br>
+    </p>
+    Brands
+    <p style="font-size: 13px;">
+        Apple (MacBook)<br>
+        Dell (XPS, Inspiron, Alienware)<br>
+        HP (Spectre, Pavilion, Omen)<br>
+        ASUS (ZenBook, ROG, TUF)<br>
+        Acer (Aspire, Predator, Swift)<br>
+        Lenovo (ThinkPad, Legion, IdeaPad)<br>
+        MSI (Stealth, Titan, GF Series)<br>
+    </p>
+        </h2>
+        <div style="padding:35px;">
+            <h1>Send us your Feedback</h1>
+        <form method="post">
+            <input type="text" id="FName" name="FName" placeholder="Name"><br><br>
+            <input type="text" id="FEmail" name="FEmail"placeholder="Email"><br><br>
+            <textarea id="FMessage" Name ="FMessage" placeholder="Messaege"></textarea><br><br>
+            <button type="submit" class="Send">Send</button>
+        </form>
+
+        </div>
+    
+    </div>
+    <div style="text-align:center;" class="copyright">© COPYRIGHT 2025 ELECTRO WIZARD ®, INC. ALL RIGHTS RESERVED.</div>
+    
 </body>
 </html>
